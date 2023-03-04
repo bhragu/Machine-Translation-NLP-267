f18179ced731ce92bcd6c4a8a562f8b5bf966995
KUSHAGRA SETH <kushagraseth.1996@gmail.com>
2023-03-04 13:53:21 -0800

    A3 Initial Commit

---
+++ hw-3/install_environment.sh
@@ -0,0 +1,4 @@
+wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+../miniconda3/bin/pip3 install -r requirements.txt
---
+++ hw-3/run.sh
@@ -0,0 +1,3 @@
+# USE below to select the GPU number on the NLP server
+export CUDA_VISIBLE_DEVICES=1
+../miniconda3/bin/python3 seq2seq.py
